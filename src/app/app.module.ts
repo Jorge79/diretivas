87200048b4bc7b2e7778002c011442cb47bb40ad
Jorge Francisco <Jorge7.9@hotmail.com>
2022-06-04 16:41:25 -0300

    Criando uma diretiva de estrutura (ngElse)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ExemploNgContentComponent } from './exemplo-ng-content/exemplo-ng-conte
 import { FundoAmareloDirective } from './shared/fundo-amarelo.directive';
 import { DiretivasCustomizadasComponent } from './diretivas-customizadas/diretivas-customizadas.component';
 import { HighlightMouseDirective } from './shared/highlight-mouse.directive';
+import { NgElseDirective } from './shared/ng-else.directive';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { HighlightMouseDirective } from './shared/highlight-mouse.directive';
     FundoAmareloDirective,
     DiretivasCustomizadasComponent,
     HighlightMouseDirective,
+    NgElseDirective,
   ],
   imports: [BrowserModule, NgbModule, FormsModule],
   providers: [],
